e076f40734fde1d2349c7b9eed6266644740b795
Quoc Viet <vietvmq@smartgift.vn>
2022-10-28 16:57:49 +0700

    set default footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,49 +5,47 @@
       <div class="row">
         <div class="col-md-6 col-lg-4 col-sm-6 mb-5 mb-lg-0 text-center text-sm-left mr-auto">
           <div class="footer-widget">
-            <h4 class="mb-4">E-Shop</h4>
-            <p class="lead">Iste dolores iure quis excepturi, deserunt praesentium.</p>
+            <h4 class="mb-4">Cửa hàng trái cây</h4>
+            <p class="lead">Cung cấp các loại trái cây tươi, sạch nhất thị trường</p>
 
             <div class="">
-              <p class="mb-0"><strong>Location : </strong>North Punjab ,INDIA</p>
-              <p><strong>Support Email : </strong> support@email.com</p>
+              <p class="mb-0"><strong>Location : </strong>140 Lê Trọng Tấn, Q. Tân Phú,  TP.HCM</p>
+              <p><strong>Email : </strong>viethuydoan@email.com</p>
             </div>
           </div>
         </div>
 
         <div class="col-md-6 col-lg-2 col-sm-6 mb-5 mb-lg-0 text-center text-sm-left">
           <div class="footer-widget">
-            <h4 class="mb-4">Category</h4>
+            <h4 class="mb-4">Loại</h4>
             <ul class="pl-0 list-unstyled mb-0">
-              <li><a href="#">Men's Fashion</a></li>
-              <li><a href="#">Women's Fashion</a></li>
-              <li><a href="#">Kids Fashion</a></li>
-              <li><a href="#">Accessories</a></li>
-              <li><a href="#">Shoe Collection</a></li>
+              <li><a href="#">Trái cây theo mùa</a></li>
+              <li><a href="#">Trái cây theo khu vực</a></li>
+              <li><a href="#">Trái cây đặc sản</a></li>
+              <li><a href="#">Trái cây nhập khẩu</a></li>
             </ul>
           </div>
         </div>
 
         <div class="col-md-6 col-lg-2 col-sm-6 mb-5 mb-lg-0 text-center text-sm-left">
           <div class="footer-widget">
-            <h4 class="mb-4">Useful Link</h4>
+            <h4 class="mb-4">Chi tiết</h4>
             <ul class="pl-0 list-unstyled mb-0">
-              <li><a href="#">News &amp; Tips</a></li>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Support</a></li>
-              <li><a href="#">Our Shop</a></li>
-              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Về chúng tôi</a></li>
+              <li><a href="#">Hỗ trợ</a></li>
+              <li><a href="#">Cửa hàng</a></li>
+              <li><a href="#">Liên hệ</a></li>
             </ul>
           </div>
         </div>
 
         <div class="col-md-6 col-lg-3 col-sm-6 text-center text-sm-left">
           <div class="footer-widget">
-            <h4 class="mb-4">Opening Hours</h4>
+            <h4 class="mb-4">Thời gian làm việc</h4>
             <ul class="pl-0 list-unstyled mb-5">
-              <li class="d-lg-flex justify-content-between">Monday-Friday <span>8.00-20.00</span></li>
-              <li class="d-lg-flex justify-content-between">Saturday <span>10.00-20.00</span></li>
-              <li class="d-lg-flex justify-content-between">Sunday <span>12-20.00</span></li>
+              <li class="d-lg-flex justify-content-between">Thứ 2 - Thứ 6 <span>8.00-20.00</span></li>
+              <li class="d-lg-flex justify-content-between">Thứ 7 <span>10.00-20.00</span></li>
+              <li class="d-lg-flex justify-content-between">Chủ Nhật<span>12-20.00</span></li>
             </ul>
 
             <h5>Call Now : +(000) 000-000</h5>
@@ -57,22 +55,4 @@
     </div>
   </footer>
 
-
-  <div class="footer-btm py-4 ">
-    <div class="container">
-      <div class="row ">
-        <div class="col-lg-6">
-          <p class="copyright mb-0 ">@ Copyright Reserved to therichpost &amp; made by <a href="https://therichpost.com/">therichpost</a></p>
-        </div>
-        <div class="col-lg-6">
-          <ul class="list-inline mb-0 footer-btm-links text-lg-right mt-2 mt-lg-0">
-            <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
-            <li class="list-inline-item"><a href="#">Terms &amp; Conditions</a></li>
-            <li class="list-inline-item"><a href="#">Cookie Policy</a></li>
-            <li class="list-inline-item"><a href="#">Terms of Sale</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
