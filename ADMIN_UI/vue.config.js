32633648d37008dae7093db56392796b3706826c
Viet <91690030+Vietvo0210@users.noreply.github.com>
2022-12-07 15:29:23 +0700

    DELETE ADMIN_UI

--- ADMIN_UI/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  devServer: {
-    host: 'fruitadmin.hufi.local',
-    allowedHosts: "all",
-   },
-  transpileDependencies: true,
-  lintOnSave: false,
-})
