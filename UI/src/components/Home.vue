9f45b8da3c63a9947675b23a620b61645ccca6ac
Quoc Viet <vietvmq@smartgift.vn>
2022-12-08 23:40:29 +0700

    fetch Api product List for screen Product

--- UI/src/components/Home.vue
+++ UI/src/components/Home.vue
@@ -1,103 +1,38 @@
 <template>
-  <div id="carouselExampleControls" class="carousel slide main-slider slider" data-ride="carousel">
-    <div class="carousel-inner">
-      <div  class="slider-item carousel-item active" style="background-image:url('https://dacsandalat.com.vn/wp-content/uploads/2014/06/qua-hong.jpg')">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-6 col-12 offset-lg-6 offset-md-6">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div   class="slider-item carousel-item" style="background-image:url('https://2.bp.blogspot.com/-rm4WSmTHJsM/Unkgq-rt26I/AAAAAAAAFBg/Wbaj_1wuNxc/s1600/ip+006.JPG')">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-6 col-12 offset-lg-6 offset-md-6">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div   class="slider-item carousel-item" style="background-image:url('https://2.bp.blogspot.com/-rm4WSmTHJsM/Unkgq-rt26I/AAAAAAAAFBg/Wbaj_1wuNxc/s1600/ip+006.JPG');
-      background-position:50%;background-repeat:no-repeat;">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-6 col-12 offset-lg-6 offset-md-6">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-  <section class="category section pt-3 pb-0">
-
-  </section>
-
-  <section class="section products-main">
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-lg-8">
+        <div class="col-lg-12">
           <div class="title text-center">
-            <h2 >Cửa hàng trái cây tươi Hufi-Fruits</h2>
-            <p>Viet Huy Doan</p>
+            <h2 >CỬA HÀNG TRÁI CÂY TƯƠI HUFI_FRUITS</h2>
           </div>
         </div>
       </div>
-
-
-      <div class="row">
-        <div class="col-lg-3 col-12 col-md-6 col-sm-6 mb-5" >
-          <div class="product">
+      <br/>
+      <br/>
+      <div class="row" >
+        <div class="col-lg-3" v-for="post in posts" :key="post.id">
+          <div class="product" >
             <div class="product-wrap">
-              <a href="#"><img class="img-fluid w-100 mb-3 img-first" src="http://cdn.tgdd.vn/Files/2022/03/19/1421206/cay-mang-cut-trong-o-dau-bao-lau-thi-co-trai-202203191428342280.jpg" alt="product-img" /></a>
-              <a href="#"><img class="img-fluid w-100 mb-3 img-second" src="http://cdn.tgdd.vn/Files/2022/03/19/1421206/cay-mang-cut-trong-o-dau-bao-lau-thi-co-trai-202203191428342280.jpg" alt="product-img" /></a>
+              <a href="#"><img
+                class="img-fluid w-100 mb-3 img-first"
+                :src="post.thumbnail"
+                alt="okke"/>
+              </a>
             </div>
-
-            <span class="onsale">Sale</span>
             <div class="product-hover-overlay">
               <a href="#"><i class="tf-ion-android-cart"></i></a>
               <a href="#"><i class="tf-ion-ios-heart"></i></a>
             </div>
 
             <div class="product-info">
-              <h2 class="product-title h5 mb-0"><a href="#">Măng cụt</a></h2>
-              <del>160.000vnđ/1kg<pre wp-pre-tag-5=""></pre></del>
+              <h3 class="product-title h5 mb-0">{{post.title}}</h3>
               <span class="price">
-                        120.000vnđ/1kg
+                {{post.price}}x
                     </span>
             </div>
           </div>
         </div>
-
-        <div class="col-lg-3 col-12 col-md-6 col-sm-6 mb-5">
-          <div class="product">
-            <div class="product-wrap">
-              <a href="#"><img class="img-fluid w-100 mb-3 img-first" src="http://cdn.tgdd.vn/Files/2017/05/23/985304/nhung-cong-dung-tuyet-voi-cua-thanh-long-ban-nen-biet-202109171144120943.jpg" alt="product-img" /></a>
-              <a href="#"><img class="img-fluid w-100 mb-3 img-second" src="http://cdn.tgdd.vn/Files/2017/05/23/985304/nhung-cong-dung-tuyet-voi-cua-thanh-long-ban-nen-biet-202109171144120943.jpg" alt="product-img" /></a>
-            </div>
-
-            <div class="product-hover-overlay">
-              <a href="#"><i class="tf-ion-android-cart"></i></a>
-              <a href="#"><i class="tf-ion-ios-heart"></i></a>
-            </div>
-
-            <div class="product-info">
-              <h2 class="product-title h5 mb-0"><a href="#">Thanh long</a></h2>
-              <span class="price">
-                        40.000vnđ/1kg
-                    </span>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-12 col-md-6 col-sm-6 mb-5" >
+        <!-- <div class="col-lg-3 col-12 col-md-6 col-sm-6 mb-5" >
           <div class="product">
             <div class="product-wrap">
               <a href="#"><img class="img-fluid w-100 mb-3 img-first" src="https://2.bp.blogspot.com/-rm4WSmTHJsM/Unkgq-rt26I/AAAAAAAAFBg/Wbaj_1wuNxc/s1600/ip+006.JPG" alt="product-img" /></a>
@@ -173,7 +108,7 @@
               <a href="#"><i class="tf-ion-ios-heart"></i></a>
             </div>
 
-            <div class="product-info">
+            <div class="product-info" >
               <h2 class="product-title h5 mb-0"><a href="#">Ổi</a></h2>
               <span class="price">
                         30.000vnđ/1kg
@@ -229,10 +164,9 @@
                     </span>
             </div>
           </div>
-        </div>
+        </div>-->
       </div>
     </div>
-  </section>
   <!-- /portfolio -->
 
   <section>
@@ -244,7 +178,7 @@
         </div>
         <div class="col-lg-6">
             <span class="h5 deal">Giảm lên đến 50% </span>
-            <h2 class="mt-3 text-black">Các loại trái cây Việt Nam</h2>
+            <h3 class="mt-3 text-black">Các loại trái cây Việt Nam</h3>
             <p class="text-md mt-3 text-white">Nhanh chóng đặt hàng ngay nào!</p>
             <a href="#" class="btn btn-main"><i class="ti-bag mr-2"></i>Shop Now </a>
           </div>
@@ -295,3 +229,34 @@
     </div>
   </section>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+
+  methods: {
+    async getData() {
+      try {
+        let response = await fetch("http://192.168.1.26:8085/api/product-list/");
+        this.posts = await response.json();;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+
+  created() {
+    this.getData();
+  },
+};
+</script>
+<style>
+h2{
+  padding: 40px;
+  background-color:powderblue;
+  color: #1b1e21;
+}
+</style>
