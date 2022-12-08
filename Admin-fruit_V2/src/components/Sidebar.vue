d332b4aabf8d494e9259b71f3c60cac1435419fb
kaokoqdoan <88839933+kaokoqdoan@users.noreply.github.com>
2022-12-08 14:29:04 +0700

    UI_admin_v2

---
+++ Admin-fruit_V2/src/components/Sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-navigation-drawer v-model="drawer" app>
+    <v-img
+      height="140"
+      class="pa-4"
+      src="https://preview.pixlr.com/images/800wm/1439/2/1439104804.jpg"
+    >
+      <div class="text-center">
+        <v-avatar class="mb-4" color="grey darken-1" size="64">
+          <v-img
+            aspect-ratio="30"
+            src=""
+          />
+        </v-avatar>
+        <h2 class="white--text">Admin</h2>
+      </div>
+    </v-img>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-item v-for="[icon, text,link] in links" :key="icon" :to="link" link>
+        <v-list-item-icon>
+          <v-icon>{{ icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ text }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  props: ["drawer"],
+  data() {
+    return {
+      links: [
+        ["mdi-microsoft-windows", "Dashboard","/"],
+        ["mdi-account", "Profile","/Profile"],
+        ["mdi-clipboard-list-outline", "Products","/Product"],
+        ["mdi-card-account-details-outline", "Orders","/Order"],
+        ["mdi-account-details-outline", "Customer","/Customer"],
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ Admin-fruit_V2/src/views/Product.vue
@@ -0,0 +1,312 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    sort-by="calories"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>Product</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              Add New
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.id"
+                      label="ID"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.title"
+                      label="Title"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.price"
+                      label="price"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.discount"
+                      label="Discount"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.thumbnail"
+                      label="Thumbnail"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.description"
+                      label="Description"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="close"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="save"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this product?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn
+        color="primary"
+        @click="initialize"
+      >
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+  export default {
+    name:"Product",
+    data: () => ({
+      dialog: false,
+      dialogDelete: false,
+      headers: [
+      {
+          text: 'ID',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        
+        { text: 'Title', value: 'title' },
+        { text: 'Price', value: 'price' },
+        { text: 'Discount', value: 'discount' },
+        { text: 'Thumbnail', value: 'thumbnail' },
+        { text: 'Description', value: 'description' },
+        { text: 'Actions', value: 'actions', sortable: false },
+      ],
+      desserts: [],
+      editedIndex: -1,
+      editedItem: {
+        id: '',
+        title: 0,
+        price: 0,
+        discount: 0,
+        thumbnail: 0,
+        description: 0,
+      },
+      defaultItem: {
+        id: '',
+        title: 0,
+        price: 0,
+        discount: 0,
+        thumbnail: 0,
+        description: 0,
+      },
+    }),
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Product' : 'Edit Product'
+      },
+    },
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
+    },
+    created () {
+      this.initialize()
+    },
+    methods: {
+      initialize () {
+        this.desserts = [
+          {
+            id: '1',
+            title: 0,
+            price: 0,
+            discount: 0,
+            thumbnail: 0,
+            description: 0,
+          },
+          {
+            id: '2',
+            title: 0,
+            price: 0,
+            discount: 0,
+            thumbnail: 0,
+            description: 0,
+          },
+          {
+            id: '3',
+            title: 0,
+            price: 0,
+            discount: 0,
+            thumbnail: 0,
+            description: 0,
+          },
+          {
+            id: '4',
+            title: 0,
+            price: 0,
+            discount: 0,
+            thumbnail: 0,
+            description: 0,
+          },
+          {
+            id: '5',
+            title: 0,
+            price: 0,
+            discount: 0,
+            thumbnail: 0,
+            description: 0,
+          },
+          {
+            id: '6',
+            title: 0,
+            price: 0,
+            discount: 0,
+            thumbnail: 0,
+            description: 0,
+          },
+        ]
+      },
+      editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+      deleteItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+      deleteItemConfirm () {
+        this.desserts.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        } else {
+          this.desserts.push(this.editedItem)
+        }
+        this.close()
+      },
+    },
+  }
+</script>
+
