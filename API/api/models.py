f9e6726d3469da68a8a4c4a2c682785a4f94551f
Quoc Viet <vietvmq@smartgift.vn>
2022-12-08 21:48:45 +0700

    update API

--- API/api/models.py
+++ API/api/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+from keras.models import load_model
+
+import cv2
+import numpy as np
+import requests
+from PIL import Image
+import urllib.request
+import skimage.io
+
 # Create your models here.
 
 class Product(models.Model):
@@ -10,3 +19,49 @@ class Product(models.Model):
     description =models.CharField(max_length=2000)
     class Meta:
         db_table='Product'
+
+class Customer(models.Model):
+    fullname = models.CharField(max_length=50)
+    email = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    password = models.CharField(max_length=32)
+    class Meta:
+        db_table='Customer'
+
+class Feedback(models.Model):
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    email = models.CharField(max_length=250)
+    phone_number = models.CharField(max_length=20)
+    note = models.CharField(max_length=1000)
+    class Meta:
+        db_table='Feedback'
+
+class Galery(models.Model):
+    product_id = models.IntegerField()
+    thumbnail = models.CharField(max_length=500)
+    class Meta:
+        db_table='Galery'
+
+class Orders(models.Model):
+    customer_id = models.IntegerField()
+    fullname = models.CharField(max_length=50)
+    email = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+    note = models.CharField(max_length=1000)
+    order_date=models.CharField(max_length=15)
+    status=models.IntegerField()
+    total_money=models.IntegerField()
+    class Meta:
+        db_table='Orders'
+
+class Order_details(models.Model):
+    order_id = models.IntegerField()
+    product_id = models.IntegerField()
+    price = models.IntegerField()
+    num = models.IntegerField()
+    total_money = models.IntegerField()
+    class Meta:
+        db_table='Order_details'
--- API/api/views.py
+++ API/api/views.py
@@ -1,40 +1,25 @@
 from django.shortcuts import render
-
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
-from .serializers import ProductSerializer
-from .models import Product
-
-from django.shortcuts import render
 from rest_framework import status
 from django.http import JsonResponse
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-
 from keras.models import load_model
-
 import cv2
 import numpy as np
 import requests
 from PIL import Image
 import urllib.request
 import skimage.io
-
-
+from imaplib import _Authenticator
+from .serializers import ProductSerializer,CustomerSerializer,FeedbackSerializer,GalerySerializer,Order_detailsSerializer,OdersSerializer
+from .models import Product,Galery,Feedback,Order_details,Orders,Customer
+from django.http import JsonResponse
+from rest_framework.generics import ListCreateAPIView
+from django.contrib.auth.models import User
 
 # Create your views here.
-
-"""@api_view(['GET'])
-def apiOverview(request):
-    api_urls = {
-        'List': '/product-list/',
-        'Detail View': '/product-detail/<int:id>/',
-        'Create': '/product-create/',
-        'Update': '/product-update/<int:id>/',
-        'Delete': '/product-detail/<int:id>/',
-    }
-    return Response(api_urls);
-"""
+# Product
 
 @api_view(['GET'])
 def ShowAll(request):
@@ -61,7 +46,7 @@ def CreateProduct(request):
 
 
 
-@api_view(['PUT'])
+@api_view(['POST'])
 def updateProduct(request, pk):
     product = Product.objects.get(id=pk)
     serializer = ProductSerializer(instance=product, data=request.data)
@@ -77,6 +62,240 @@ def deleteProduct(request, pk):
     product.delete()
 
     return Response('Items delete successfully!')
+
+#Customer
+
+@api_view(['GET'])
+def ShowAll_Customer(request):
+    customer = Customer.objects.all()
+    serializer = CustomerSerializer(customer, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def ViewCustomer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    serializer = CustomerSerializer(customer, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def CreateCustomer(request):
+    serializer = CustomerSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def updateCustomer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    serializer = CustomerSerializer(instance=customer, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def deleteCustomer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    customer.delete()
+
+    return Response('Items delete successfully!')
+
+#Feedback
+
+@api_view(['GET'])
+def ShowAll_Feedback(request):
+    feedback = Feedback.objects.all()
+    serializer = FeedbackSerializer(feedback, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def ViewFeedback(request, pk):
+    feedback = Feedback.objects.get(id=pk)
+    serializer = FeedbackSerializer(feedback, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def CreateFeedback(request):
+    serializer = FeedbackSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def updateFeedback(request, pk):
+    feedback = Feedback.objects.get(id=pk)
+    serializer = FeedbackSerializer(instance=feedback, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def deleteFeedback(request, pk):
+    feedback = Feedback.objects.get(id=pk)
+    feedback.delete()
+
+    return Response('Items delete successfully!')
+
+
+#Galery
+@api_view(['GET'])
+def ShowAll_Galery(request):
+    galery = Galery.objects.all()
+    serializer = GalerySerializer(galery, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def ViewGalery(request, pk):
+    galery = Galery.objects.get(id=pk)
+    serializer = GalerySerializer(galery, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def CreateGalery(request):
+    serializer = GalerySerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def updateGalery(request, pk):
+    galery = Galery.objects.get(id=pk)
+    serializer = GalerySerializer(instance=galery, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def deleteGalery(request, pk):
+    galery = Galery.objects.get(id=pk)
+    galery.delete()
+
+    return Response('Items delete successfully!')
+
+#Orders
+@api_view(['GET'])
+def ShowAll_Orders(request):
+    orders = Orders.objects.all()
+    serializer = OdersSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def ViewOrders(request, pk):
+    orders = Orders.objects.get(id=pk)
+    serializer = OdersSerializer(orders, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def CreateOrders(request):
+    serializer = OdersSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def updateOrders(request, pk):
+    orders = Orders.objects.get(id=pk)
+    serializer = OdersSerializer(instance=orders, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def deleteOrders(request, pk):
+    orders = Orders.objects.get(id=pk)
+    orders.delete()
+
+    return Response('Items delete successfully!')
+
+#Order_details
+@api_view(['GET'])
+def ShowAll_Order_details(request):
+    order_details = Order_details.objects.all()
+    serializer = Order_detailsSerializer(order_details, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def Vieworder_details(request, pk):
+    order_details = Order_details.objects.get(id=pk)
+    serializer = Order_detailsSerializer(order_details, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def Createorder_details(request):
+    serializer = Order_detailsSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def updateorder_details(request, pk):
+    order_details = Order_details.objects.get(id=pk)
+    serializer = Order_detailsSerializer(instance=order_details, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def deleteorder_details(request, pk):
+    order_details = Order_details.objects.get(id=pk)
+    order_details.delete()
+
+    return Response('Items delete successfully!')
+
+
+def login(seft,request):
+            email=request.POST['email']
+            password = request.POST['password']
+            bool_answer = User.objects.filter(yourtablefield="admin").exists()
+            intakes = bool_answer.objects.all().filter(email,password)
+
+            for intake in intakes:
+
+                if intake.email==email and intake.password==password:
+                     return True
+                else:
+                     return False
+
+
 
 class GetPredictedResult(ListCreateAPIView):
     vgg16_model=load_model('/Users/admin/Desktop/Hocky6/Nhandangraucu_mayhoc/GUI/Viet_Huy_Doan_Traicay.model')
