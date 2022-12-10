ddf178e7a7bcd2ff6bd9888a2b31d7b69b7b6bfe
Quoc Viet <vietvmq@smartgift.vn>
2022-12-11 01:55:06 +0700

    delete with id

--- Admin-fruit/src/views/Product.vue
+++ Admin-fruit/src/views/Product.vue
@@ -5,13 +5,15 @@
     sort-by="calories"
     class="elevation-1"
   >
+    <template v-slot:item.sn="{ index }">
+      {{ index + 1 }}
+    </template>
     <template v-slot:top>
       <v-toolbar flat>
         <v-toolbar-title>Product</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
         <v-dialog v-model="dialog" max-width="500px">
-
           <template v-slot:activator="{ on, attrs }">
             <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
               Add New
@@ -24,7 +26,6 @@
             </v-card-title>
 
             <v-card-text>
-              {{ data[0].id }}
               <v-container>
                 <v-row>
                   <v-col cols="12" sm="6" md="4">
@@ -60,23 +61,10 @@
             </v-card-actions>
           </v-card>
         </v-dialog>
-
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5">Are you sure you want to delete this product?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
       </v-toolbar>
     </template>
 
     <template v-slot:item.actions="{ item }">
-      {{ item.id }}
       <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
       <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
     </template>
@@ -89,13 +77,6 @@
 import axios from "axios";
 import { ref } from 'vue'
 export default {
-  props: {
-    item: {
-      type: Object,
-      default: () => {
-      }
-    }
-  },
   name: "Products",
   data: () => ({
     selected: "",
@@ -166,14 +147,16 @@ export default {
       this.dialog = true
     },
     deleteItem (item) {
-      this.editedItem = Object.assign({}, item)
-      console.log(this.data.row)
-      this.dialogDelete = true
-    },
-    deleteItemConfirm () {
-      console.log(this.data[0].id)
-      axios.delete("http://192.168.1.26:8085/api/product-delete/")
-      this.closeDelete()
+      axios.delete("http://192.168.1.26:8085/api/product-delete/" + item.id + '/')
+      console.log(item.id)
+      alert("  " + item.title +" ĐƯỢC XÓA THÀNH CÔNG!")
+      this.data = [] //RESET DATA
+      axios
+        .get("http://192.168.1.26:8085/api/product-list/" )
+        .then((response) => {
+          this.data = response.data;
+        })
+        .catch((err) => alert(err));
     },
     close () {
       this.dialog = false
