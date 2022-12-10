837c326b9e49b85752d31eae22fc6ef3313d6df8
Quoc Viet <vietvmq@smartgift.vn>
2022-12-10 20:50:40 +0700

    api post product, pending API update, delete

--- Admin-fruit/src/views/Product.vue
+++ Admin-fruit/src/views/Product.vue
@@ -6,122 +6,61 @@
     class="elevation-1"
   >
     <template v-slot:top>
-      <v-toolbar
-        flat
-      >
+      <v-toolbar flat>
         <v-toolbar-title>Product</v-toolbar-title>
-        <v-divider
-          class="mx-4"
-          inset
-          vertical
-        ></v-divider>
+        <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
-        <v-dialog
-          v-model="dialog"
-          max-width="500px"
-        >
+        <v-dialog v-model="dialog" max-width="500px">
+
           <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              color="primary"
-              dark
-              class="mb-2"
-              v-bind="attrs"
-              v-on="on"
-            >
+            <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
               Add New
             </v-btn>
           </template>
+
           <v-card>
             <v-card-title>
               <span class="text-h5">{{ formTitle }}</span>
             </v-card-title>
 
             <v-card-text>
+              {{ data[0].id }}
               <v-container>
                 <v-row>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.id"
-                      label="ID"
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.title" label="Title"></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.price" label="price"></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.discount" label="Discount"
                     ></v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.title"
-                      label="Title"
-                    ></v-text-field>
+
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.thumbnail" label="Thumbnail"></v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.price"
-                      label="price"
-                    ></v-text-field>
+
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.description" label="Description"></v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.discount"
-                      label="Discount"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.thumbnail"
-                      label="Thumbnail"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.description"
-                      label="Description"
-                    ></v-text-field>
-                  </v-col>
+
                 </v-row>
               </v-container>
             </v-card-text>
 
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="close"
-              >
-                Cancel
-              </v-btn>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="save"
-              >
-                Save
-              </v-btn>
+              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+
+              <v-btn color="blue darken-1" text @click="postData">SAVE</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
+
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
             <v-card-title class="text-h5">Are you sure you want to delete this product?</v-card-title>
@@ -135,34 +74,28 @@
         </v-dialog>
       </v-toolbar>
     </template>
+
     <template v-slot:item.actions="{ item }">
-      <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-        small
-        @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
+      {{ item.id }}
+      <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+      <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
     </template>
     <template v-slot:no-data>
-      <v-btn
-        color="primary"
-        @click="initialize"
-      >
-        Reset
-      </v-btn>
+      <v-btn color="primary" @click="initialize">Reset</v-btn>
     </template>
   </v-data-table>
 </template>
 <script>
 import axios from "axios";
+import { ref } from 'vue'
 export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => {
+      }
+    }
+  },
   name: "Products",
   data: () => ({
     selected: "",
@@ -175,23 +108,23 @@ export default {
       { text: "Description", value: "" },
       { text: "Actions", value: 'actions', sortable: false },
     ],
-    editedIndex: -1,
+    editedIndex: ref(-1),
     data: [],
     editedItem: {
       id: '',
-      title: 0,
-      price: 0,
-      discount: 0,
-      thumbnail: 0,
-      description: 0,
+      title: '',
+      price: '',
+      discount: '',
+      thumbnail: '',
+      description: '',
     },
     defaultItem: {
       id: '',
-      title: 0,
-      price: 0,
-      discount: 0,
-      thumbnail: 0,
-      description: 0,
+      title: '',
+      price: '',
+      discount: '',
+      thumbnail: '',
+      description: '',
     },
     computed: {
       formTitle () {
@@ -209,6 +142,7 @@ export default {
   },
   methods: {
     getData() {
+      console.log(this.item)
       axios
         .get("http://192.168.1.26:8085/api/product-list/" )
         .then((response) => {
@@ -216,46 +150,54 @@ export default {
         })
         .catch((err) => alert(err));
     },
+    postData(){
+      console.log('INSERT DATA')
+      axios.post("http://192.168.1.26:8085/api/product-create/", this.editedItem)
+        .then(function (response) {
+          //close form
+          this.dialog(false)
+        console.log(response)
+        console.log("POST SUCCESS!")
+      })
+    },
+   editItem (item) {
+     this.editedIndex = this.data.indexOf(item.id)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+    deleteItem (item) {
+      this.editedItem = Object.assign({}, item)
+      console.log(this.data.row)
+      this.dialogDelete = true
+    },
+    deleteItemConfirm () {
+      console.log(this.data[0].id)
+      axios.delete("http://192.168.1.26:8085/api/product-delete/")
+      this.closeDelete()
+    },
+    close () {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+    closeDelete () {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+    save (item) {
+      console.log(item)
+      this.editItem()
+      this.close()
+    },
   },
 
   mounted() {
     this.getData();
-  },
-  editItem (item) {
-    this.editedIndex = this.desserts.indexOf(item)
-    this.editedItem = Object.assign({}, item)
-    this.dialog = true
-  },
-  deleteItem (item) {
-    this.editedIndex = this.desserts.indexOf(item)
-    this.editedItem = Object.assign({}, item)
-    this.dialogDelete = true
-  },
-  deleteItemConfirm () {
-    this.desserts.splice(this.editedIndex, 1)
-    this.closeDelete()
-  },
-  close () {
-    this.dialog = false
-    this.$nextTick(() => {
-      this.editedItem = Object.assign({}, this.defaultItem)
-      this.editedIndex = -1
-    })
-  },
-  closeDelete () {
-    this.dialogDelete = false
-    this.$nextTick(() => {
-      this.editedItem = Object.assign({}, this.defaultItem)
-      this.editedIndex = -1
-    })
-  },
-  save () {
-    if (this.editedIndex > -1) {
-      Object.assign(this.desserts[this.editedIndex], this.editedItem)
-    } else {
-      this.desserts.push(this.editedItem)
-    }
-    this.close()
   },
 };
 </script>
