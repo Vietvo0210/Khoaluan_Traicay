3a065a93be8fa97633ef71caf7da57131378cab4
Thân Văn Huy <92750807+thanvanhuy@users.noreply.github.com>
2023-01-08 15:21:09 +0700

    Update views.py

--- API/api/views.py
+++ API/api/views.py
@@ -1,8 +1,8 @@
 from imaplib import _Authenticator
 from django.shortcuts import render
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-<<<<<<< HEAD
 
 from .serializers import ProductSerializer,CustomerSerializer,FeedbackSerializer,GalerySerializer,Order_detailsSerializer,OdersSerializer
 from .models import Product,Galery,Feedback,Order_details,Orders,Customer
@@ -18,28 +18,16 @@ from rest_framework.generics import ListCreateAPIView
 
 
 ## import lbr load model
-=======
-from rest_framework import status
-from django.http import JsonResponse
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
->>>>>>> 6ede5db404ecb9e0c67ff725f10a8090dffccd9d
 from keras.models import load_model
+
 import cv2
 import numpy as np
 import skimage.io
-<<<<<<< HEAD
-=======
-from imaplib import _Authenticator
->>>>>>> 6ede5db404ecb9e0c67ff725f10a8090dffccd9d
 from .serializers import ProductSerializer,CustomerSerializer,FeedbackSerializer,GalerySerializer,Order_detailsSerializer,OdersSerializer
 from .models import Product,Galery,Feedback,Order_details,Orders,Customer
 from django.http import JsonResponse
 from rest_framework.generics import ListCreateAPIView
 from django.contrib.auth.models import User
-<<<<<<< HEAD
-=======
-
->>>>>>> 6ede5db404ecb9e0c67ff725f10a8090dffccd9d
 # Create your views here.
 # Product
 
@@ -69,7 +57,7 @@ def CreateProduct(request):
 
 
 
-@api_view(['POST'])
+@api_view(['PUT'])
 def updateProduct(request, pk):
     product = Product.objects.get(id=pk)
     serializer = ProductSerializer(instance=product, data=request.data)
@@ -316,242 +304,8 @@ def deleteorder_details(request, pk):
 
     return Response('Items delete successfully!')
 
-#Customer
-
-@api_view(['GET'])
-def ShowAll_Customer(request):
-    customer = Customer.objects.all()[:10]
-    serializer = CustomerSerializer(customer, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def ViewCustomer(request, pk):
-    customer = Customer.objects.get(id=pk)
-    serializer = CustomerSerializer(customer, many=False)
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def CreateCustomer(request):
-    serializer = CustomerSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-
-@api_view(['POST'])
-def updateCustomer(request, pk):
-    customer = Customer.objects.get(id=pk)
-    serializer = CustomerSerializer(instance=customer, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def deleteCustomer(request, pk):
-    customer = Customer.objects.get(id=pk)
-    customer.delete()
-
-    return Response('Items delete successfully!')
-
-#Feedback
-
-@api_view(['GET'])
-def ShowAll_Feedback(request):
-    feedback = Feedback.objects.all()[:10]
-    serializer = FeedbackSerializer(feedback, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def ViewFeedback(request, pk):
-    feedback = Feedback.objects.get(id=pk)
-    serializer = FeedbackSerializer(feedback, many=False)
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def CreateFeedback(request):
-    serializer = FeedbackSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-
-@api_view(['POST'])
-def updateFeedback(request, pk):
-    feedback = Feedback.objects.get(id=pk)
-    serializer = FeedbackSerializer(instance=feedback, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def deleteFeedback(request, pk):
-    feedback = Feedback.objects.get(id=pk)
-    feedback.delete()
-
-    return Response('Items delete successfully!')
-
-
-#Galery
-@api_view(['GET'])
-def ShowAll_Galery(request):
-    galery = Galery.objects.all()[:10]
-    serializer = GalerySerializer(galery, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def ViewGalery(request, pk):
-    galery = Galery.objects.get(id=pk)
-    serializer = GalerySerializer(galery, many=False)
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def CreateGalery(request):
-    serializer = GalerySerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-
-@api_view(['POST'])
-def updateGalery(request, pk):
-    galery = Galery.objects.get(id=pk)
-    serializer = GalerySerializer(instance=galery, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def deleteGalery(request, pk):
-    galery = Galery.objects.get(id=pk)
-    galery.delete()
-
-    return Response('Items delete successfully!')
-
-#Orders
-@api_view(['GET'])
-def ShowAll_Orders(request):
-    orders = Orders.objects.all()[:10]
-    serializer = OdersSerializer(orders, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def ViewOrders(request, pk):
-    orders = Orders.objects.get(id=pk)
-    serializer = OdersSerializer(orders, many=False)
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def CreateOrders(request):
-    serializer = OdersSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-
-@api_view(['POST'])
-def updateOrders(request, pk):
-    orders = Orders.objects.get(id=pk)
-    serializer = OdersSerializer(instance=orders, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def deleteOrders(request, pk):
-    orders = Orders.objects.get(id=pk)
-    orders.delete()
-
-    return Response('Items delete successfully!')
-
-#Order_details
-@api_view(['GET'])
-def ShowAll_Order_details(request):
-    order_details = Order_details.objects.all()[:10]
-    serializer = Order_detailsSerializer(order_details, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def Vieworder_details(request, pk):
-    order_details = Order_details.objects.get(id=pk)
-    serializer = Order_detailsSerializer(order_details, many=False)
-    return Response(serializer.data)
-
-
-@api_view(['POST'])
-def Createorder_details(request):
-    serializer = Order_detailsSerializer(data=request.data)
-
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-
-@api_view(['POST'])
-def updateorder_details(request, pk):
-    order_details = Order_details.objects.get(id=pk)
-    serializer = Order_detailsSerializer(instance=order_details, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def deleteorder_details(request, pk):
-    order_details = Order_details.objects.get(id=pk)
-    order_details.delete()
-
-    return Response('Items delete successfully!')
-
-
-def login(seft,request):
-            email=request.POST['email']
-            password = request.POST['password']
-            bool_answer = User.objects.filter(yourtablefield="admin").exists()
-            intakes = bool_answer.objects.all().filter(email,password)
-
-            for intake in intakes:
-
-                if intake.email==email and intake.password==password:
-                     return True
-                else:
-                     return False
-
-
-
 class GetPredictedResult(ListCreateAPIView):
-    vgg16_model=load_model('E:/huyvip_123/KhoaLuan_NhanDangHoaQua/Viet_Huy_Doan_Traicay/Viet_Huy_Doan_Traicay.model')
+    vgg16_model=load_model('C:/Users/Demo201/Documents/GitHub/Viet_Huy_Doan_Traicay.model/')
     class_names = ["ambarella", "avocado ", "banana", "coconut", "custardapple", "dragonfruit", "durian", "guava", "jackfruit" ,
                   "lychee","mango","mangosteen","persimmon","pineapple","plumcot",
                   "plums","pomelo", "rambutan","saboche","tomato", "watermelon"
