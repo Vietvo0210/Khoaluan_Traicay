33e3f017b658721aae2004bbd7d49b995042c710
Thân Văn Huy <92750807+thanvanhuy@users.noreply.github.com>
2023-02-13 13:51:27 +0700

    css_new

--- UI/src/components/NewDetail.vue
+++ UI/src/components/NewDetail.vue
@@ -1,40 +1,114 @@
 <template>
   <div>
-    <div>
-      <h1>ĂN TR&Aacute;I C&Acirc;Y Đ&Uacute;NG C&Aacute;CH DỄ M&Agrave; KH&Oacute;?</h1>
+    <div class="text-top">
+      <h1>Eating fruit the right way is easy but difficult</h1>
     </div>
-    <div>
-      <p>- Tr&aacute;i c&acirc;y l&agrave; nguồn dinh dưỡng tự nhi&ecirc;n tốt nhất bổ sung sức khỏe v&agrave; sức đề kh&aacute;ng.Tuy nhi&ecirc;n để c&oacute; thể c&oacute; thể hấp thụ dinh dưỡng từ tr&aacute;i c&acirc;y tốt nhất kh&ocirc;ng phải ai cũng biết. H&atilde;y c&ugrave;ng FS TR&Aacute;I C&Acirc;Y TƯƠI t&igrave;m hiểu những điều n&ecirc;n v&agrave; kh&ocirc;ng n&ecirc;n l&agrave;m khi ăn Tr&aacute;i C&acirc;y Tươi nh&eacute;!</p>
-      <p><strong>(I) Những điều n&ecirc;n l&agrave;m khi ăn Tr&aacute;i C&acirc;y Tươi</strong></p>
-      <p>- Khi ăn Tr&aacute;i C&acirc;y Tươi c&aacute;c bạn n&ecirc;n lưu &yacute; những vấn đề n&ecirc;n l&agrave;m để c&oacute; thể hấp thụ dinh dưỡng - kho&aacute;ng chất một c&aacute;ch tốt nhất nh&eacute;</p>
-      <p><u>a) N&ecirc;n ăn ngay khi vừa cắt</u></p>
-      <p>- Hương vị cũng như những chất dinh dưỡng tốt nhất của Tr&aacute;i c&acirc;y sẽ bị giảm dần theo thời gian nếu để qu&aacute; l&acirc;u khi bị cắt gọt hoặc d&ugrave;ng l&agrave;m &eacute;p nước. V&igrave; thế, bạn n&ecirc;n ăn ngay để tr&aacute;nh bị mất chất dinh dưỡng.&nbsp;</p>
-      <p>- Trong trường hợp bất khả kh&aacute;ng bắt buộc phải chuẩn bị trước, bạn c&oacute; ng&acirc;m hoa quả cắt trong dung dịch nước muối nhạt (1%). L&agrave;m như thế, lượng vitamin - chất dinh dưỡng v&agrave; c&aacute;c kho&aacute;ng chất sẽ được giữ nguy&ecirc;n ở mức tối đa nhất.</p>
-      <p><img src="https://file.hstatic.net/200000217865/file/hop_dua_hau_dua_luoi_man_5b90dcb1e37a49f69628e669d3f0cd0b_grande.jpg"></p>
-      <p><u>b) N&ecirc;n ưu ti&ecirc;n ăn tr&aacute;i c&acirc;y tươi</u></p>
-      <p>- H&agrave;m lượng Chất Xơ trong tr&aacute;i c&acirc;y tươi rất nhiều - lượng chất xơ n&agrave;y rất tốt cho hệ ti&ecirc;u h&oacute;a, gi&uacute;p ngăn ngừa c&aacute;c bệnh li&ecirc;n quan đến vấn đề ti&ecirc;u h&oacute;a như kh&oacute; ti&ecirc;u, t&aacute;o b&oacute;n. Ăn tr&aacute;i c&acirc;y tươi, nguy&ecirc;n tr&aacute;i l&agrave; c&aacute;ch hiệu quả nhất để c&oacute; thể hấp thụ trực tiếp chất xơ.</p>
-      <p>- Tuy nhi&ecirc;n, với nhiều người rất th&iacute;ch thưởng thức nước &eacute;p hoa quả v&igrave; nghĩ đ&acirc;y l&agrave; phần bổ dưỡng v&agrave; tinh t&uacute;y trong tr&aacute;i c&acirc;y sẽ được &eacute;p ra. Nhưng, khi &eacute;p như thế v&ocirc; t&igrave;nh l&agrave;m mất đi một lượng lớn chất xơ đang kể. N&ecirc;n khi uống nước &eacute;p, bạn n&ecirc;n thưởng thức từ từ, chia th&agrave;nh nhiều ngụm nhỏ để c&oacute; thể nước bọt h&ograve;a c&ugrave;ng nước &eacute;p, tạo điều kiện cho cơ thể hấp thu c&aacute;c dưỡng chất tốt hơn.</p>
-      <p><img src="https://file.hstatic.net/200000217865/file/dau_hq_500gr_5f39988588f74e129d472f2a70b90704_grande.jpg"></p>
-      <p><u>c) Tr&aacute;nh xa c&aacute;c loại tr&aacute;i c&acirc;y c&ocirc;ng nghiệp</u></p>
-      <p>- Tr&aacute;i c&acirc;y tươi rất tốt cho cơ thể v&agrave; sức khỏe trong m&ugrave;a dịch bệnh, nhất l&agrave; với t&igrave;nh h&igrave;nh dịch Covid chưa được kiểm so&aacute;t ho&agrave;n to&agrave;n, việc sử dụng tr&aacute;i c&acirc;y tươi l&agrave; điều hết sức cần thiết. Tuy nhi&ecirc;n với những tr&aacute;i c&acirc;y &ldquo;c&ocirc;ng nghiệp&rdquo; lại t&aacute;c dụng ngược ho&agrave;n to&agrave;n c&oacute; thể g&acirc;y t&aacute;c hại đến cơ thể.</p>
-      <p>- C&aacute;c loại Tr&aacute;i c&acirc;y sấy, l&agrave;m mứt hoặc đ&oacute;ng chai nước &eacute;p đều chứa rất &iacute;t Vitamin v&agrave; chất xơ, trong khi lượng chất bảo quản v&agrave; đường h&oacute;a học rất cao. N&ecirc;n cần lưu &yacute; khi sử dụng tr&aacute;i c&acirc;y &ldquo;C&ocirc;ng nghiệp&rdquo; nh&eacute;</p>
-      <p>&nbsp;</p>
-      <p><strong>(II) Những điều kh&ocirc;ng n&ecirc;n l&agrave;m khi ăn Tr&aacute;i C&acirc;y Tươi</strong></p>
-      <p>- C&aacute;c bạn cũng n&ecirc;n lưu &yacute; những vấn đề cần n&ecirc;n tr&aacute;nh khi sử dụng tr&aacute;i c&acirc;y tươi nh&eacute;.</p>
-      <p><u>a) Kh&ocirc;ng n&ecirc;n ăn tr&aacute;i c&acirc;y sau bữa ăn ch&iacute;nh</u></p>
-      <p>- T&iacute;nh chất của tr&aacute;i c&acirc;y l&agrave; được ti&ecirc;u h&oacute;a nhanh hơn cơm v&agrave; thức ăn. Như thế, khi ăn bữa ch&iacute;nh, thay v&igrave; chuyển h&oacute;a v&agrave; đẩy xuống ruột, hoa quả lại bị cơm hay c&aacute;c thức phẩm kh&aacute;c chưa kịp ti&ecirc;u chặn lại. L&uacute;c đ&oacute;, tr&aacute;i c&acirc;y v&agrave; thức ăn sẽ bị l&ecirc;n men, l&agrave;m cho bạn đầy hơi, trướng bụng v&agrave; kh&oacute; ti&ecirc;u.</p>
-      <p>- Khi đ&oacute;, lượng tinh bột v&agrave; Carbonhydrate trong tr&aacute;i c&acirc;y tương đối lớn. Như vậy ch&uacute;ng sẽ t&iacute;ch tụ trong dạ d&agrave;y sẽ ảnh hưởng tới hiệu suất của hệ ti&ecirc;u h&oacute;a. Ngo&agrave;i ra, c&oacute; nhiều loại tr&aacute;i c&acirc;y c&oacute; t&iacute;nh chất ngọt cao v&agrave; chứa nhiều đường. Kết hợp c&ugrave;ng thực phẩm trong bữa ăn sẽ l&agrave;m tăng cao đường huyết v&agrave; kh&ocirc;ng tốt cho người bị tiểu đường.</p>
-      <p><u>b) Kh&ocirc;ng n&ecirc;n/Hạn chế chế biến Tr&aacute;i c&acirc;y tươi</u></p>
-      <p>- C&aacute;c c&aacute;ch thức chế biến tr&aacute;i c&acirc;y như: sấy kh&ocirc;, x&agrave;o nấu, nướng đều &ldquo;tước lấy&rdquo; hầu như to&agrave;n bộ dinh dưỡng trong c&acirc;y. V&igrave; thế, bạn n&ecirc;n hạn chế chế biến để c&oacute; thể tận dụng to&agrave;n bộ chất dinh dưỡng của sản phẩm nh&eacute;.</p>
-      <p>- Nếu muốn kết hợp th&agrave;nh m&oacute;n ăn, n&ecirc;n trộn tr&aacute;i c&acirc;y th&agrave;nh Salad với lượng sốt v&agrave; dầu dấm tối thiểu sẽ l&agrave; phương &aacute;n tốt nhất.</p>
-      <p><u>c) Kh&ocirc;ng n&ecirc;n để Tr&aacute;i c&acirc;y tươi v&agrave;o tủ lạnh ăn dần</u></p>
-      <p>- C&aacute;c loại vitamin bổ dưỡng trong tr&aacute;i c&acirc;y như Vitamin C, K, D..c&oacute; thể bị mất dần dưới t&aacute;c dụng của &aacute;nh s&aacute;ng, kh&ocirc;ng kh&iacute;, nhiệt độ&hellip;</p>
-      <p>-&nbsp;Gọt vỏ, cắt miếng tr&aacute;i c&acirc;y để l&acirc;u sẽ l&agrave;m cho qu&aacute; tr&igrave;nh oxy h&oacute;a v&agrave; nhiễm khuẩn tr&aacute;i c&acirc;y nhanh v&agrave; dễ d&agrave;ng hơn. Một số tr&aacute;i c&acirc;y như t&aacute;o c&oacute; thể ăn cả vỏ (nếu th&iacute;ch) th&igrave; phải ng&acirc;m rửa sạch trước ăn, nếu gọt vỏ th&igrave; n&ecirc;n ăn ngay. T&aacute;o mất lớp vỏ bảo vệ sẽ dễ bị oxy h&oacute;a nếu để l&acirc;u trong kh&ocirc;ng kh&iacute;.</p>
-      <p><u>d) Những lưu &yacute; kh&aacute;c cần tr&aacute;nh khi ăn tr&aacute;i c&acirc;y</u></p>
-      <p>-&nbsp;Kh&ocirc;ng n&ecirc;n ăn tr&aacute;i c&acirc;y v&agrave;o buổi tối, v&igrave; chứa một lượng lớn đường&nbsp;fructozo, g&oacute;p phần hỗ trợ cơ thể t&iacute;ch trữ năng lượng v&agrave; hấp thụ chất b&eacute;o. Ăn tr&aacute;i c&acirc;y buổi tối sẽ g&oacute;p phần l&agrave;m bạn &ldquo;b&eacute;o bụng&rdquo;</p>
-      <p>- Kh&ocirc;ng n&ecirc;n cắt hoặc gọt vỏ tr&aacute;i c&acirc;y rồi để ra ngo&agrave;i kh&ocirc;ng kh&iacute;, c&aacute;c kho&aacute;ng chất v&agrave; Vitamin c&oacute; trong tr&aacute;i c&acirc;y sẽ bị oxi h&oacute;a bởi m&ocirc;i trường xung quanh, trực tiếp l&agrave;m mất dưỡng chất của tr&aacute;i c&acirc;y.</p>
-      <p><strong>(III) Gợi &yacute; Những m&oacute;n tr&aacute;i c&acirc;y n&ecirc;n thử trong th&aacute;ng 3</strong></p>
-      <p>- ❣️Những ng&agrave;y trong tuần th&aacute;ng 3, tiết trời xu&acirc;n sang m&aacute;t mẻ kh&ocirc;ng qu&aacute; oi bức như m&ugrave;a hạ, kh&ocirc;ng qu&aacute; kh&ocirc; như m&ugrave;a thu. Thời tiết th&iacute;ch hợp để dạo phố v&agrave; c&ugrave;ng thưởng những loại tr&aacute;i c&acirc;y đặc biệt ngon chỉ c&oacute; trong những ng&agrave;y th&aacute;ng 3. C&ugrave;ng <strong>FS&nbsp;</strong>điểm qua top 5 loại tr&aacute;i c&acirc;y n&ecirc;n thưởng thức trong th&aacute;ng 3 nh&eacute;.❣️</p>
+
+    <div class="border1">
+      <p>
+        <img class="img"
+          src="https://file.hstatic.net/200000217865/file/hop_dua_hau_dua_luoi_man_5b90dcb1e37a49f69628e669d3f0cd0b_grande.jpg">
+      </p>
+      <div class="content">
+        <p class="title">
+          Eating fruit the right way is easy but difficult ?
+        </p>
+        <p class="text">
+          Fruit is the best natural source of nutrients to supplement health and resistance. However, not everyone knows
+          to be able to absorb nutrients from the best fruit. Let's learn the do's and don'ts when eating Fresh Fruit!
+          <br>
+          Should be eaten immediately after cutting
+          <br>
+          - The taste as well as the best nutrients of the Fruit will be reduced over time if left for too long when cut
+          or used as juice. Therefore, you should eat immediately to avoid loss of nutrients.
+          <br>
+          - In case of force majeure, you must prepare in advance, you can soak the cut fruit in a solution of light
+          brine (1%). By doing so, the amount of vitamins - nutrients and minerals will be kept to the maximum extent.
+          <br>
+          Prefer to eat fresh fruit
+          <br>
+          - Fiber content in fresh fruit is very much - this amount of fiber is very good for the digestive system,
+          helping to prevent diseases related to digestive problems such as indigestion, constipation. Eating fresh,
+          whole fruit is the most effective way to get the direct absorption of fiber.
+        </p>
+      </div>
+    </div>
+    <div class="border1">
+      <p>
+        <img class="img"
+          src="https://cafefcdn.com/thumb_w/650/203337114487263232/2021/7/10/photo1625877427243-1625877427373309415802.jpg">
+      </p>
+      <div class="content">
+        <p class="title">
+          Find the link to consume durian for the mountainous Khanh Hoa.
+        </p>
+        <p class="text">
+          The mountainous district of Khanh Son, Khanh Hoa province is preparing for the durian harvest, with an
+          expected output of thousands of tons. However, due to the impact of the Covid-19 epidemic, the consumption of
+          this fruit risks many difficulties. Right now, Khanh Hoa province is implementing solutions to connect, avoid
+          disruption, and ensure the consumption of agricultural products for mountainous people.
+
+          Durian is a specialty fruit of Khanh Son mountainous district, Khanh Hoa province. Durian trees have been
+          popularly grown for the past 20 years on the valley land - the transition between the Central Highlands and
+          the coastal plain. The climate and soil are suitable for durians with golden rice and delicious flat seeds.
+          Durian here is still ripe "out of phase", harvested 1-2 months later than other localities.
+        </p>
+      </div>
+    </div>
+    <div class="border1">
+      <p>
+        <img class="img" src="https://hoaquafuji.com/storage/app/media/cam-canh-gia-bao-nhieu-0-12.jpg">
+      </p>
+      <div class="content">
+        <p class="title">
+          How to choose oranges ?
+        </p>
+        <p class="text">
+          Orange :
+          <br>
+          - Shape: choose neat fruits, not crooked, not big or small, fresh fruit knobs.
+          <br>
+          - Orange peel: orange peel has a natural shine
+          <br>
+          - Color: choose the fruits with natural color, turn yellow and have a little green tint. Natural ripe oranges
+          are usually yellow at the bottom of the fruit and spread upwards. Do not choose fruits with yellow spots
+          scattered all around because they are ripe oranges.
+          <br>
+          Benefits of Cam
+          <br>
+          - Oranges with Citrate help slow down the formation of kidney stones and reduce the risk of kidney diseases
+          <br>
+          - Prevent Viruses from entering: create a good immune system for the body to protect you from being infected
+          with Viruses, especially during the time when the Covid epidemic has not been completely controlled.
+          <br>
+          Increase fiber intake, reduce cholesterol and risk of atherosclerosis.
+          <br>
+          - Strengthen eyes and supplement nutrients for the brain
+        </p>
+      </div>
+    </div>
+    <div class="border1">
+      <p>
+        <img class="img" src="https://danviet.mediacdn.vn/296231569849192448/2021/6/20/1-162416358852638444731.jpg">
+      </p>
+      <div class="content">
+        <p class="title">
+          China buys 2.5 million tons of Vietnamese fruit, half of which is dragon fruit !
+        </p>
+        <p class="text">
+          According to a report of the Ministry of Agriculture and Rural Development, in the first 5 months of 2021,
+          China bought up to 1.2 million tons of dragon fruit from Vietnam, up 138% over the same period in 2020,
+          reaching 62.18% of the year plan.
+          <br>
+          Only the output of Vietnamese dragon fruit exported to China accounted for half of the amount of fruit China
+          bought from Vietnam in the first five months of 2021.
+          <br>
+          Up to now, Vietnam has had 9 types of fresh fruit officially exported to China, including: mango, dragon
+          fruit, banana, longan, lychee, watermelon, rambutan, jackfruit and mangosteen.
+          <br>
+          Annually, the number of fresh fruit exports to China is 3.3 - 3.5 million tons/year.
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -46,5 +120,43 @@ export default {
 </script>
 
 <style scoped>
+.text-top {
+  margin-top: 10px;
+}
 
+.border1 {
+  display: flex;
+  padding: 32px 20px;
+  border-top: 3px;
+  border-style: inset;
+  border-bottom: none;
+  border-left: none;
+  border-right: none;
+  background-color: beige;
+}
+
+.img {
+  height: 300px;
+  width: 350px;
+  margin-top: 15px;
+}
+
+.content {
+  padding-left: 20px;
+  margin-top: 10px;
+  font-family: 'Times New Roman', Times, serif;
+  color: #565656;
+}
+
+.title {
+  font-size: 23px;
+  font-weight: bold;
+  text-align: center;
+}
+
+.text {
+  font-size: 18px;
+  line-height: 25px;
+  text-align: justify;
+}
 </style>
--- UI/src/components/NewView.vue
+++ UI/src/components/NewView.vue
@@ -5,7 +5,7 @@
           src="https://file.hstatic.net/200000217865/article/web_-_cach_an_trai_cay_dung_cach_b5a0f03ee47d4c378ac7b554f68b867c_grande.jpg"
              alt="ĂN TRÁI CÂY ĐÚNG CÁCH DỄ MÀ KHÓ?">
       <router-link to="/news/detail">
-        <a class="nav-link">ĂN TRÁI CÂY ĐÚNG CÁCH DỄ MÀ KHÓ?</a>
+        <a class="nav-link">Eating fruit the right way is easy but difficult?</a>
       </router-link>
     </div>
   </div>
